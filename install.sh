0fd711950d91ccc51691cbc57805b764fab445a3
Sharvesh93 <sharvesh10001@gmail.com>
2026-05-30 15:53:14 +0000

    Adding currently using packages

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# All the required or currently using packages
+
+sudo pacman -S --needed \
+    base-devel \
+    git \
+    neovim \
+    tmux \
+    kitty \
+    fish \
+    nautilus \
+    firefox \
+    pipewire \
+    pipewire-pulse \
+    rofi \
+    waybar \
+    swaync \
+    awww \
+    fastfetch \
+    grim \
+    slurp
