f0ce0664ed0b440a1e85713c85c8e0089c09c33f
Sharvesh93 <sharvesh10001@gmail.com>
2026-06-06 22:52:17 +0530

    Updated the install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
 
 # All the required or currently using packages
+os=/etc/os-release
+if [ $os = "Arch" ]; then
+    sudo pacman -S --needed \
+        base-devel \
+        git \
+        neovim \
+        tmux \
+        kitty \
+        fish \
+        nautilus \
+        firefox \
+        pipewire \
+        pipewire-pulse \
+        rofi \
+        waybar \
+        swaync \
+        awww \
+        fastfetch \
+        grim \
+        slurp
+fi 
 
-sudo pacman -S --needed \
-    base-devel \
-    git \
-    neovim \
-    tmux \
-    kitty \
-    fish \
-    nautilus \
-    firefox \
-    pipewire \
-    pipewire-pulse \
-    rofi \
-    waybar \
-    swaync \
-    awww \
-    fastfetch \
-    grim \
-    slurp
+# Install yay
+if [ ! -d "$HOME/.local/share/yay" ]; then
+    git clone https://aur.archlinux.org/yay.git /tmp/yay
+    cd /tmp/yay
+    makepkg -si
+    cd ..
+    rm -rf /tmp/yay
+fi
+
+# Install needed drivers for nvidia gpu
+if [ $os = "Arch" ]; then
+    sudo pacman -S --needed \
+        nvidia-open \
+        nvidia-utils \
+        nvidia-settings
+fi
