34747612f791acfd729b673aecc52e602797f025
Sharvesh93 <sharvesh10001@gmail.com>
2026-07-07 22:34:20 +0530

    Hypridle

--- config/hypr/modules/autostart.lua
+++ config/hypr/modules/autostart.lua
@@ -1,5 +1,6 @@
 hl.on("hyprland.start", function () 
    hl.exec_cmd("waybar")
+   hl.exec_cmd("hypridle")
    hl.exec_cmd("awww-daemon")
    hl.exec_cmd("swaync")
    hl.exec_cmd("hyprctl setcursor Bibata-Modern-Ice 20")
